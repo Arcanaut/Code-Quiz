fc3ca83dbee8257e0ba6ebbbb48db3ffbddcf51c
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2021-11-16 15:10:18 -0600

    fixed answer feedback
    fixed feedback visibility by moving currentQuestionIndex++ from renderQuestions() to checkAnswer(),
    and set them on a timer with setTimeout() to make them disappear shortly after.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-
 let currentQuestionIndex = 0;
-
+const time = questions.length * 15;
 
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
@@ -19,63 +18,62 @@ const title = document.getElementById("title");
 var feedbackC = document.getElementById("answerCorrect")
 var feedbackW = document.getElementById("answerWrong")
 //answerCorrect.setAttribute.(hidden);
+document.getElementsByClassName("feedback")
 feedbackC.style.visibility = "hidden";
 feedbackW.style.visibility = "hidden";
 // answerCorrect.style.display="hidden";
 // answerWrong.style.display="hidden";
 //each question has scoped variables for each answer choice so that it can be used multiple times without affecting each other
 
-let questions = [
-    {
+let questions = [{
         question: "String values must be enclosed in _____ when being assigned to variables",
         choiceA: "commas",
-        choiceB:"curly brackets",
-        choiceC:"quotes",
-        choiceD:"brackets",
-        correct:"quotes",
+        choiceB: "curly brackets",
+        choiceC: "quotes",
+        choiceD: "brackets",
+        correct: "quotes",
     },
     {
         question: "A very useful tool during development and debugging for print content is ____",
         choiceA: "for loops",
         choiceB: "console.log",
         choiceC: "terminal bash",
-        choiceD:"Javascript",
+        choiceD: "Javascript",
         correct: "console.log",
-},
-{
-    question: "Arrays in Javascript can be used to store ____",
-    choiceA: "numbers and strings",
-    choiceB: "other arrays",
-    choiceC: "booleans",
-    choiceD: "All of the above",
-    correct: "All of the above",
-},
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____",
+        choiceA: "numbers and strings",
+        choiceB: "other arrays",
+        choiceC: "booleans",
+        choiceD: "All of the above",
+        correct: "All of the above",
+    },
     {
         question: "Commonly used data types include all of the following EXCEPT _____",
-        choiceA:  "strings",
+        choiceA: "strings",
         choiceB: "alerts",
         choiceC: "booleans",
         choiceD: "numbers",
-        correct: "C",
-},
-{
-    question: "The condition in an if/else statement is enclosed in _____",
-    choiceA: "parenthesis",
-    choiceB: "curly brackets",
-    choiceC: "quotes",
-    choiceD: "square brackets",
-    correct: "parenthesis",
-}
+        correct: "booleans",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed in _____",
+        choiceA: "parenthesis",
+        choiceB: "curly brackets",
+        choiceC: "quotes",
+        choiceD: "square brackets",
+        correct: "parenthesis",
+    }
 ]
 
 
-let lastQuestion = questions.length-1;
+let lastQuestion = questions.length - 1;
 //this goes through the various questions in the array. 
 
-var q;
 
-function renderQuestions(){
-    q = questions[currentQuestionIndex]
+function renderQuestions() {
+    let q = questions[currentQuestionIndex]
     //displays the current question and answers
     title.innerHTML = q.question;
     A.innerHTML = q.choiceA;
@@ -87,28 +85,52 @@ function renderQuestions(){
     C.onclick = checkAnswer
     D.onclick = checkAnswer
     //proceeds to next question
-    
-    currentQuestionIndex++
     console.log(currentQuestionIndex);
 
 };
 
 //compares the answer submitted to the correct one
-function checkAnswer(event){
+function checkAnswer(event) {
     let q = questions[currentQuestionIndex]
+    //    feedbackC.style.visibility = "hidden";
+    // feedbackW.style.visibility = "hidden";
+    console.log(q)
+    if (event.target.innerHTML === q.correct) {
+        feedbackC.style.visibility = "visible";
 
-    console.log(event.target.innerHTML)
-    if (event.target.innerHTML === q.correct) {feedbackC.style.visibility="visible";}
-    else (feedbackW.style.visibility="visible");
+        setTimeout(function () {
+            feedbackC.style.visibility = "hidden";
+        }, 2000)
+    } else {
+        feedbackW.style.visibility = "visible"
+        setTimeout(function () {
+            feedbackW.style.visibility = "hidden";
+        }, 2000)
+    }
 
-renderQuestions()
-console.log(currentQuestionIndex);
+    currentQuestionIndex++
+
+    if(currentQuestionIndex  === questions.length){ 
+        quizEnd()
+    } else {
+        renderQuestions()
+    }
+
+
+
+
+
+
 };
 
-// start.style.display ="none";
-renderQuestions();
-start.style.display ="block";
+function quizEnd(){
 
+}
+
+// start.style.display ="none";
+
+start.style.display = "block";
+renderQuestions()
 //start quiz
 
 /*function startQuiz(){
@@ -122,3 +144,4 @@ start.style.display ="block";
 //add class remove class in css
 
 //for each loop
+document.getElementById("highscore");
